3c19937bbb15575046bd853cf5c1394f22e7b35f
burdinekristy <burdinekristy28@gmail.com>
2025-08-09 13:27:26 +0000

    update

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template
+from flask_restful import Api
+from resources.recipe import RecipeListResource, RecipeResource, RecipePublishResource
 
 app = Flask(__name__)
+api = Api(app)
 
+api.add_resource(RecipeListResource, '/recipes')
+api.add_resource(RecipeResource, '/recipes/<int:recipe_id>')
+api.add_resource(RecipePublishResource, '/recipes/<int:recipe_id>/publish')
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
